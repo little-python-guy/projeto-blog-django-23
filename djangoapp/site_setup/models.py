3832b9ffe45c47c53a80327f35a42f1d1fdfa8e2
little-python-guy <matat201@gmail.com>
2024-07-04 19:12:59 -0300

    Adicionando opção de escolha de favicon

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -36,5 +36,10 @@ class SiteSetup(models.Model):
     show_pagination = models.BooleanField(default=True)
     show_footer = models.BooleanField(verbose_name='Mostrar Footer', default=True)
 
+    favicon = models.ImageField(
+        upload_to='assets/favicon/%Y/%m/',
+        blank=True, default=''
+    )
+
     def __str__(self):
         return self.title
