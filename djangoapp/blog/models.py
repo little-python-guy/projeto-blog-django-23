ca52fc80c551f956f8119837fb878c3fa48ba59a
little-python-guy <matat201@gmail.com>
2024-07-13 11:16:22 -0300

    Redimensionando imagens enviadas no Post

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from utils.images import resize_image
 from utils.rands import slugify_new
 
 
@@ -151,4 +152,16 @@ class Post(models.Model):
         if not self.slug:
             self.slug = slugify_new(self.title, 4)
 
-        return super().save(*args, **kwargs)
+        current_cover_name = str(self.cover.name)
+
+        super_save = super().save(*args, **kwargs)
+
+        cover_changed = False
+
+        if self.cover:
+            cover_changed = current_cover_name != self.cover.name
+
+        if cover_changed:
+            resize_image(self.cover, 900, optimize=True, quality=70)
+
+        return super_save
