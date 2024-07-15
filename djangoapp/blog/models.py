faa4419ac92e4ea3f7a5310dfbd124c6bb0c7a98
little-python-guy <matat201@gmail.com>
2024-07-15 20:29:44 -0300

    Adicionando model 'PostAttachment'

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,7 +1,28 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django_summernote.models import AbstractAttachment
 from utils.images import resize_image
 from utils.rands import slugify_new
+
+
+class PostAttachment(AbstractAttachment):
+
+    def save(self, *args, **kwargs):
+
+        if not self.name:
+            self.name = self.file.name
+
+        current_file_name = str(self.file.name)
+        super_save = super().save(*args, **kwargs)
+        file_change = False
+
+        if self.file:
+            file_change = current_file_name != self.file.name
+
+        if file_change:
+            resize_image(self.file, 900, True, 70)
+
+        return super_save
 
 
 class Tag(models.Model):
