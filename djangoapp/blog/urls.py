9f5924b845090640077a4250e0cba1bbdde8e9dc
little-python-guy <matat201@gmail.com>
2024-07-17 15:14:11 -0300

    Adicionando link para categoria

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,4 +1,4 @@
-from blog.views import created_by, index, page, post
+from blog.views import category, created_by, index, page, post
 from django.urls import path
 
 app_name = 'blog'
@@ -8,4 +8,5 @@ urlpatterns = [
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
     path('created_by/<int:author_pk>/', created_by, name='created_by'),
+    path('category/<slug:slug>/', category, name='category'),
 ]
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -39,6 +39,25 @@ def created_by(request, author_pk):
         }
     )
 
+def category(request, slug):
+    
+    posts = (
+        Post.objects.get_published() # type: ignore
+        .filter(category__slug=slug)
+    )
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
 
 def page(request, slug):
 
